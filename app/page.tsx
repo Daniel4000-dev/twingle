f520b6e8c39578619d6676fb433d4536046a2ba4
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2024-03-06 01:23:06 +0100

    about to add chart

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h flex-row items-center justify-between w-screen">
-        <div className="w-[20vw] h-screen bg-black text-white">
-          <div className='flex flex-col flex-1 m-4'>
-            <div className='flex justify-between'>
-                <h2 className="">Twingle</h2>
-                <h2 className="text-sm text-gray-300">Tier 1</h2>
-            </div>
+    <div className="flex flex-row items-center justify-between w-screen">
+      <div className="w-[20vw] min-h-screen bg-black text-white">
+        <div className="flex flex-col flex-1 m-4">
+          <div className="flex justify-between">
+            <h2 className="">Twingle</h2>
+            <h2 className="text-sm text-gray-300">Tier 1</h2>
+          </div>
 
-            <div className="hover:bg-gray-50 hover:bg-opacity-5 hover:rounded-lg">
-              <h2>go away</h2>
-            </div>  
+          <div className="hover:bg-gray-50 hover:bg-opacity-5 hover:rounded-lg">
+            <h2>go away</h2>
           </div>
         </div>
+      </div>
 
-        <div className="w-[80vw] flex flex-col items-center bg-gray-100 text-black">
-         <div className="w-[76vw] mt-4">
-         <div className="flex justify-between mb-12">
+      <div className="w-[80vw] min-h-screen flex flex-1 flex-col items-center bg-gray-100 text-black">
+        <div className="w-[76vw] mt-4">
+          <div className="flex justify-between mb-12">
             <h2 className="text-3xl">Summary</h2>
             <div>
               <h2>jeddy Teep</h2>
@@ -28,27 +28,31 @@ export default function Home() {
           <div className="flex gap-4 justify-between mb-4">
             <div className="h-full w-[19vw] pl-4 bg-white rounded-lg flex items-center">
               <div>
-                <p className='text-lg p-1'>ok</p>
+                <p className="text-lg p-1">ok</p>
               </div>
               <div className="h-full w-45 bg-white text-start  justifycenter p-2 rounded-lg">
                 <h4 className="text-[12px]">Revenue</h4>
                 <h2 className="text-xl">₦128,939,938</h2>
-                <h4 className="text-[12px] text-green-500">8% from last month</h4>
+                <h4 className="text-[12px] text-green-500">
+                  8% from last month
+                </h4>
               </div>
             </div>
             <div className="h-full w-[19vw] pl-4 bg-white rounded-lg flex items-center">
               <div>
-                <p className='text-lg p-1'>ok</p>
+                <p className="text-lg p-1">ok</p>
               </div>
               <div className="h-full w-45 bg-white text-start  justifycenter p-2 rounded-lg">
                 <h4 className="text-[12px]">Sign ups</h4>
                 <h2 className="text-xl">123,847</h2>
-                <h4 className="text-[12px] text-green-500">6% from last month</h4>
+                <h4 className="text-[12px] text-green-500">
+                  6% from last month
+                </h4>
               </div>
             </div>
             <div className="h-full w-[19vw] pl-4 bg-white rounded-lg flex items-center">
               <div>
-                <p className='text-lg p-1'>ok</p>
+                <p className="text-lg p-1">ok</p>
               </div>
               <div className="h-full w-45 bg-white text-start  justifycenter p-2 rounded-lg">
                 <h4 className="text-[12px]">Active users</h4>
@@ -58,7 +62,7 @@ export default function Home() {
             </div>
             <div className="h-full w-[19vw] pl-4 bg-white rounded-lg flex items-center">
               <div>
-                <p className='text-lg p-1'>ok</p>
+                <p className="text-lg p-1">ok</p>
               </div>
               <div className="h-full w-45 bg-white text-start  justifycenter p-2 rounded-lg">
                 <h4 className="text-[12px]">Matches</h4>
@@ -69,57 +73,110 @@ export default function Home() {
           </div>
 
           <div className="flex gap-4 justify-between mb-4">
-            <div className="w-[53vw] bg-white rounded-lg">
-              p
-            </div>
+            <div className="w-[53vw] bg-white rounded-lg">p</div>
             <div className="w-[23vw] bg-white rounded-lg">
-             <div className="w-[22.9vw] flex flex-col items-center text-start">
-             <div className="w-[21vw] mb-4 pt-3">
-              <h2 className="textstart">Summary of complaints</h2>
-             </div>
-              <div className="flex flex-col gap-2 mb-4 items-center">
-              <div className="w-[21vw]  rounded-lg shadow-sm shadow-purple-200">
-                <h2 className="bg-purple-200 text-sm rounded-t-lg pl-6 p-1">Inappropriate language</h2>
-                <div className="flex justify-around m-2 pt-1 pb-1">
-                  <h2>552 <span className='text-sm'>reports</span></h2>
-                  <p>--</p>
-                  <h2 className="text-sm">in the past month</h2>
+              <div className="w-[22.9vw] flex flex-col items-center text-start">
+                <div className="w-[21vw] mb-4 pt-3">
+                  <h2 className="textstart">Summary of complaints</h2>
+                </div>
+                <div className="flex flex-col gap-2 mb-4 items-center">
+                  <div className="w-[21vw]  rounded-lg shadow-sm shadow-purple-200">
+                    <h2 className="bg-purple-200 text-sm rounded-t-lg pl-6 p-1">
+                      Inappropriate language
+                    </h2>
+                    <div className="flex justify-around m-2 pt-1 pb-1">
+                      <h2>
+                        552 <span className="text-sm">reports</span>
+                      </h2>
+                      <p>--</p>
+                      <h2 className="text-sm">in the past month</h2>
+                    </div>
+                  </div>
+                  <div className="w-[21vw]  rounded-lg shadow-sm shadow-purple-200">
+                    <h2 className="bg-purple-200 text-sm rounded-t-lg pl-6 p-1">
+                      Inappropriate language
+                    </h2>
+                    <div className="flex justify-around m-2 pt-1 pb-1">
+                      <h2>
+                        552 <span className="text-sm">reports</span>
+                      </h2>
+                      <p>--</p>
+                      <h2 className="text-sm">in the past month</h2>
+                    </div>
+                  </div>
+                  <div className="w-[21vw]  rounded-lg shadow-sm shadow-purple-200">
+                    <h2 className="bg-purple-200 text-sm rounded-t-lg pl-6 p-1">
+                      Inappropriate language
+                    </h2>
+                    <div className="flex justify-around m-2 pt-1 pb-1">
+                      <h2>
+                        552 <span className="text-sm">reports</span>
+                      </h2>
+                      <p>--</p>
+                      <h2 className="text-sm">in the past month</h2>
+                    </div>
+                  </div>
+                  <div className="w-[21vw]  rounded-lg shadow-sm shadow-purple-200">
+                    <h2 className="bg-purple-200 text-sm rounded-t-lg pl-6 p-1">
+                      Inappropriate language
+                    </h2>
+                    <div className="flex justify-around m-2 pt-1 pb-1">
+                      <h2>
+                        552 <span className="text-sm">reports</span>
+                      </h2>
+                      <p>--</p>
+                      <h2 className="text-sm">in the past month</h2>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="w-[21vw]  rounded-lg shadow-sm shadow-purple-200">
-                <h2 className="bg-purple-200 text-sm rounded-t-lg pl-6 p-1">Inappropriate language</h2>
-                <div className="flex justify-around m-2 pt-1 pb-1">
-                  <h2>552 <span className='text-sm'>reports</span></h2>
-                  <p>--</p>
-                  <h2 className="text-sm">in the past month</h2>
-                </div>
-              </div>
-              <div className="w-[21vw]  rounded-lg shadow-sm shadow-purple-200">
-                <h2 className="bg-purple-200 text-sm rounded-t-lg pl-6 p-1">Inappropriate language</h2>
-                <div className="flex justify-around m-2 pt-1 pb-1">
-                  <h2>552 <span className='text-sm'>reports</span></h2>
-                  <p>--</p>
-                  <h2 className="text-sm">in the past month</h2>
-                </div>
-              </div>
-              <div className="w-[21vw]  rounded-lg shadow-sm shadow-purple-200">
-                <h2 className="bg-purple-200 text-sm rounded-t-lg pl-6 p-1">Inappropriate language</h2>
-                <div className="flex justify-around m-2 pt-1 pb-1">
-                  <h2>552 <span className='text-sm'>reports</span></h2>
-                  <p>--</p>
-                  <h2 className="text-sm">in the past month</h2>
-                </div>
-              </div>
-              </div>
-             </div>
             </div>
           </div>
 
-          <div className="bg-white">
-p
+          <div className="bg-white h-[23vh] rounded-lg flex flex-col items-center">
+            <div className='w-[72vw] mt-2 mb-2'>
+              <h2>47 pending disputes</h2>
+            </div>
+            <div className="w-[72vw] h-[17vh] overflow-auto">
+            <div className="mb-2 bg-gray-100 flex items-center justify-between w-[72vw] p-3 rounded-lg">
+              <div>
+              <h2 className="text-">
+                Chiamaka <span className='text-sm text-gray-300'>chiamaka@gmail.com</span>
+              </h2>
+              <h2 className="text-sm">
+                Hello there, i am having difficulty setting up my account. My
+                password keeps being declined each time i add it to the form.
+                what...
+              </h2>
+              </div>
+              <div className="flex flex-col text-[13px] text-gray-400">
+                <h2>19 Jan, 2022</h2>
+                <h2>10:28 AM</h2>
+                <h2></h2>
+              </div>
+            </div>
+
+            <div className="bg-gray-100 flex items-center justify-between w-[72vw] p-3 rounded-lg">
+              <div>
+              <h2 className="text-">
+                Chiamaka <span className='text-sm text-gray-300'>chiamaka@gmail.com</span>
+              </h2>
+              <h2 className="text-sm">
+                Hello there, i am having difficulty setting up my account. My
+                password keeps being declined each time i add it to the form.
+                what...
+              </h2>
+              </div>
+              <div className="flex flex-col text-[13px] text-gray-400">
+                <h2>19 Jan, 2022</h2>
+                <h2>10:28 AM</h2>
+                <h2></h2>
+              </div>
+            </div>
+            </div>
           </div>
-         </div>
         </div>
+      </div>
     </div>
   );
 }
