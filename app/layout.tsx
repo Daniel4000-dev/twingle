ca2498c8f58a7ac16eac4b5b2c47a1296c283bfa
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2024-06-02 02:39:24 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import PageWrapper from "@/components/pagewrapper";
 import { Metadata } from 'next';
-import Header from "../components/header";
-import Sidebar from "../components/sidebar";
+import Header from "@/components/header";
+import Sidebar from "@/components/sidebar";
 
 const fontSans = FontSans({
   subsets: ["latin"],
